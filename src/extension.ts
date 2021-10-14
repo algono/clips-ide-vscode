7ccc67936efd72f6d0a4ed6afe8a688649cf2e44
algono <algono@inf.upv.es>
2021-10-14 22:45:12 +0200

    Implemented basic terminal functionality (WIP) (For now using node REPL instead of CLIPS for testing)

--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,10 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-import { ChildProcess, spawn } from 'child_process';
+import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
 
 const state: {
-  clips?: ChildProcess;
+  clips?: ChildProcessWithoutNullStreams;
   writeEmitter?: vscode.EventEmitter<string>;
 } = {};
 
@@ -17,27 +17,12 @@ export function activate(context: vscode.ExtensionContext) {
 
   state.writeEmitter = writeEmitter;
 
-  let line = '';
-  const clipsPty: vscode.Pseudoterminal = {
-    onDidWrite: writeEmitter.event,
-    onDidClose: closeEmitter.event,
-    open: () => {
-      state.clips = spawn('clips');
-      state.clips.on('error', (err) => {
-        vscode.window.showErrorMessage(
-          'Fatal error. Check if CLIPS is installed.'
-        );
-        console.error('Error: ', err);
-        closeEmitter.fire();
-      });
-    },
-    close: () => {},
-    handleInput: (data) => {
+  const handleInput: (data: string) => void = (data) => {
       switch (data) {
         case '\r':
-          writeEmitter.fire('\r\n');
-          writeEmitter.fire('line: ' + line);
-          // TODO: Run command on CLIPS, and write the result
+          writeEmitter.fire('\r\n');      
+          state.clips?.stdin.write(line + '\r\n');
+          line = '';
           break;
         case '\x7f': // Backspace
           if (line.length === 0) {
@@ -52,12 +37,35 @@ export function activate(context: vscode.ExtensionContext) {
       }
       line += data;
       writeEmitter.fire(data);
+  };
+
+  let line = '';
+  const clipsPty: vscode.Pseudoterminal = {
+    onDidWrite: writeEmitter.event,
+    onDidClose: closeEmitter.event,
+    open: () => {
+      // TODO: ADAPT CODE TO CLIPS INSTEAD OF NODE REPL
+      state.clips = spawn('node', ['-i']);
+      state.clips.on('error', (err) => {
+        vscode.window.showErrorMessage(
+          'Fatal error. Check if CLIPS is installed.'
+        );
+        console.error('Error: ', err);
+        closeEmitter.fire();
+      });
+      state.clips.stdout.on('data', (res) => {
+        console.log('DATA: ', res.toString());
+        writeEmitter.fire('\r' + (res.toString() as string).replace('\n', '\r\n'));
+      });
+      state.clips.on('exit', () => closeEmitter.fire());
     },
+    close: () => {},
+    handleInput,
   };
 
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "clips-ide" is now active!');
+  //console.log('Congratulations, your extension "clips-ide" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
@@ -67,7 +75,7 @@ export function activate(context: vscode.ExtensionContext) {
     () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World from CLIPS!');
+      //vscode.window.showInformationMessage('Hello World from CLIPS!');
 
       let terminal = vscode.window.createTerminal({
         name: 'CLIPS',
