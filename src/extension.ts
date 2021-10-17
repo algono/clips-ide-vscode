109e6cc697a70c2b7b8e96d5ec3db565a0a474e4
algono <algono@inf.upv.es>
2021-10-17 14:45:14 +0200

    Change main command name and id

--- src/extension.ts
+++ src/extension.ts
@@ -157,7 +157,7 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    'clips-ide.helloWorld',
+    'clips-ide.open-clips-env',
     async () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
