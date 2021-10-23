algono/clips-ide-vscode

commit 721513a805de6abc5a7e29017844d49a187657d8

Fixed wrong error message on exit Also changed closePty to close And added onChangeActiveTerminal listener to disposables

src/ClipsRepl.ts
@@ -20,7 +20,7 @@ function colorRed(data: string) {
 
 export default class ClipsRepl {
   private clips?: IPty;
-  private started?: boolean;
+  private starting?: boolean;
   private lock?: AsyncLock;
   private terminalHasLock?: boolean;
   private lockDone?: Parameters<
@@ -65,10 +65,12 @@ export default class ClipsRepl {
         // Used to take the first line, where the version is printed
         this.redirectWriteEmitter = versionCheckEmitter;
 
+        this.starting = true;
+
         this.clips = nodepty.spawn('clips', [], {});
 
         // Idea from: https://github.com/microsoft/node-pty/issues/74#issuecomment-295520624
-        setTimeout(() => (this.started = true), 500);
+        setTimeout(() => (this.starting = false), 500);
 
         this.clips.onData((data) => {
           let sData: string = data.toString();
@@ -126,13 +128,12 @@ export default class ClipsRepl {
           }
         });
         this.clips.onExit(() => {
-          if (!this.started) {
+          if (this.starting) {
             vscode.window.showErrorMessage(
               'Fatal error. Check if CLIPS is installed.'
             );
           }
 
-          this.closePty();
           return ptyCloseEmitter.fire();
         });
         vscode.commands.executeCommand(
@@ -141,7 +142,7 @@ export default class ClipsRepl {
           true
         );
       },
-      close: this.closePty,
+      close: this.close,
       handleInput: handlerInput.handle,
     };
 
@@ -188,14 +189,12 @@ export default class ClipsRepl {
     this.terminal?.sendText('\r', false);
   }
 
-  closePty() {
+  close = () => {
     console.log('CLOSING PTY');
 
     // Make sure that the shell is actually closed
     this.clips?.kill();
     this.lockDone?.();
-
-    this.started = false;
 
     vscode.commands.executeCommand(
       'setContext',
@@ -204,7 +203,7 @@ export default class ClipsRepl {
     );
 
     this.closeEmitter.fire();
-  }
+  };
 
   createTerminal(): vscode.Terminal {
     return (this.terminal = vscode.window.createTerminal(this.terminalOptions));

src/extension.ts
@@ -68,7 +68,7 @@ export function activate(context: vscode.ExtensionContext) {
   };
 
   // If multiple CLIPS terminals are open, we should only use the last active one
-  vscode.window.onDidChangeActiveTerminal(updateActiveRepl);
+  const activeD = vscode.window.onDidChangeActiveTerminal(updateActiveRepl);
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
@@ -139,10 +139,10 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(cmdD);
   });
 
-  context.subscriptions.push(termD, mainD, docD, loadD, loadCD);
+  context.subscriptions.push(termD, mainD, docD, loadD, loadCD, activeD);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
-  state.instances.forEach((c) => c.closePty());
+  state.instances.forEach((c) => c.close());
 }

commit 90dc280b8d3b0bc4f3be941a9e243e4ddf475a31

Fixed terminal was not being closed on exit This bug was introduced in commit 721513a8 Because that was supposed to fix another bug where close was being called twice Now both bugs should be fixed

src/ClipsRepl.ts
@@ -137,6 +137,7 @@ export default class ClipsRepl {
             );
           }
 
+          this.close();
           return ptyCloseEmitter.fire();
         });
         vscode.commands.executeCommand(
@@ -145,7 +146,7 @@ export default class ClipsRepl {
           true
         );
       },
-      close: this.close,
+      close: () => this.clips?.kill(),
       handleInput: handlerInput.handle,
     };
 