fb5952b1f28d1a6e6319edd7c9e91f4ea13ed57e
algono <algono@inf.upv.es>
2021-10-23 14:03:44 +0200

    Generalized opening docs and showing editors

--- src/ClipsDocs.ts
+++ src/ClipsDocs.ts
@@ -3,24 +3,26 @@ import ClipsRepl, { commandEnded } from './ClipsRepl';
 import { RedirectData } from './logic';
 
 const docNames = ['facts', 'agenda'] as const;
-type DocNames = typeof docNames[number];
+type DocName = typeof docNames[number];
 
 class ClipsDoc {
   content: string = '';
+  private doc?: vscode.TextDocument;
 
   constructor(
-    private name: DocNames,
+    private name: DocName,
     private onDidChangeEmitter: vscode.EventEmitter<vscode.Uri>,
     private getRepl: () => ClipsRepl | undefined
   ) {}
 
   clear = () => (this.content = '');
 
-  getUri = () => `clips:${this.name}`;
+  getUriString = () => `clips:${this.name}`;
+  getUri = () => vscode.Uri.parse(this.getUriString());
 
   isVisible = () =>
     vscode.window.visibleTextEditors.some(
-      (e) => e.document.uri.toString() === this.getUri()
+      (e) => e.document.uri.toString() === this.getUriString()
     );
 
   updateDoc = () => {
@@ -50,7 +52,7 @@ class ClipsDoc {
       if (commandEnded(data)) {
         this.content = cleanDoc([this.content, prepare]);
 
-        this.onDidChangeEmitter.fire(vscode.Uri.parse(this.getUri()));
+        this.onDidChangeEmitter.fire(this.getUri());
       }
     });
 
@@ -59,28 +61,38 @@ class ClipsDoc {
       repl.redirectWriteEmitter = emitter;
     });
   };
+
+  async open() {
+    const uri = this.getUri();
+    return (this.doc = await vscode.workspace.openTextDocument(uri));
+  }
+
+  async show(options?: vscode.TextDocumentShowOptions) {
+    if (!this.doc) {
+      return;
+    }
+    return await vscode.window.showTextDocument(this.doc, options);
+  }
 }
 
 export default class ClipsDocs {
-  private docs: { [k in DocNames]: ClipsDoc };
+  private docs: { [k in DocName]?: ClipsDoc };
   myProvider;
-  private openEditors?: vscode.TextEditor[];
+  private openEditors: vscode.TextEditor[];
 
   constructor(private repl?: ClipsRepl) {
     this.myProvider = this.createProvider();
-
-    const docs: Partial<typeof this.docs> = {};
-    docNames.forEach((name) => {
-      docs[name] = new ClipsDoc(
-        name,
-        this.myProvider.onDidChangeEmitter,
-        () => this.repl
-      );
-    });
-    // TypeScript can't infer that the previous forEach filled all the required properties,
-    // so we assure the type system that it did
-    this.docs = docs as typeof this.docs;
+    this.docs = {};
+    this.openEditors = [];
   }
+
+  createDoc = (name: DocName) => {
+    return (this.docs[name] = new ClipsDoc(
+      name,
+      this.myProvider.onDidChangeEmitter,
+      () => this.repl
+    ));
+  };
 
   setRepl = (repl: ClipsRepl) => {
     this.repl = repl;
@@ -94,7 +106,7 @@ export default class ClipsDocs {
       provideTextDocumentContent(uri: vscode.Uri): string {
         const contentType = uri.path;
         if (contentType in thisObj.docs) {
-          const content = thisObj.docs[contentType as DocNames].content;
+          const content = thisObj.docs[contentType as DocName]?.content;
           console.log('PROVIDING: ', content);
           return content ?? '';
         }
@@ -105,36 +117,38 @@ export default class ClipsDocs {
 
   updateDocs = () => {
     for (const docName in this.docs) {
-      this.docs[docName as DocNames].updateDoc();
+      this.docs[docName as DocName]?.updateDoc();
     }
   };
 
+  private async openDoc(name: DocName, options?: vscode.TextDocumentShowOptions) {
+    let doc = this.docs[name];
+    if (!doc) {
+      doc = this.createDoc(name);
+    }
+
+    await doc.open();
+
+    const editor = await doc.show(options);
+
+    if (editor) {
+      this.openEditors.push(editor);
+    }
+  }
+
   async open() {
-    const factsUri = vscode.Uri.parse('clips:facts');
-    const agendaUri = vscode.Uri.parse('clips:agenda');
-
-    this.updateDocs();
-
-    const factsDoc = await vscode.workspace.openTextDocument(factsUri);
-    const agendaDoc = await vscode.workspace.openTextDocument(agendaUri);
-
-    const factsEditor = await vscode.window.showTextDocument(factsDoc, {
-      preview: false,
-      viewColumn: vscode.ViewColumn.Beside,
-    });
+    await this.openDoc('facts');
 
     // Split the previous and next document horizontally
     await vscode.commands.executeCommand('workbench.action.newGroupBelow');
 
-    const agendaEditor = await vscode.window.showTextDocument(agendaDoc, {
-      preview: false,
-    });
+    await this.openDoc('agenda');
 
     // Give focus to the original group by focusing the previous one once for each editor the extension creates
     vscode.commands.executeCommand('workbench.action.focusPreviousGroup');
     vscode.commands.executeCommand('workbench.action.focusPreviousGroup');
 
-    this.openEditors = [factsEditor, agendaEditor];
+    this.updateDocs();
   }
 
   close() {
@@ -153,9 +167,9 @@ export default class ClipsDocs {
     });
 
     for (const docName in this.docs) {
-      this.docs[docName as DocNames].clear();
+      this.docs[docName as DocName]?.clear();
     }
-    delete this.openEditors;
+    this.openEditors = [];
   }
 
   dispose() {
